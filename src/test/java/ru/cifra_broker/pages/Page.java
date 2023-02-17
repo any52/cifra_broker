23bba0e5df8c60b5ae434cfc28d8893990c207f0
Anna Moiseeva <belova_al@mail.ru>
2023-02-17 23:24:16 +0300

    Update Page.java

--- src/test/java/ru/cifra_broker/pages/Page.java
+++ src/test/java/ru/cifra_broker/pages/Page.java
@@ -2,6 +2,7 @@ package ru.cifra_broker.pages;
 
 import com.codeborne.selenide.WebDriverRunner;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,7 +10,8 @@ public class Page {
 
     public void verifyUrlPage(String expectedUrl) {
         Set<String> pages = WebDriverRunner.getWebDriver().getWindowHandles();
-        WebDriverRunner.getWebDriver().switchTo().window(pages.stream().toList().get(pages.size()-1));
+        WebDriverRunner.getWebDriver().switchTo().window(pages.stream()
+                .collect(Collectors.toList()).get(pages.size()-1));
         String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
         assertEquals(expectedUrl, currentUrl);
     }
